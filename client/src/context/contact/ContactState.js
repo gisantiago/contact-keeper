4ffbd9eef35af9ee1c3c6b4b34cdbef1e1fab4d9
gisantiago <gisantiago@live.com>
2019-06-21 12:18:04 -0400

    Delete Contact From UI

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -52,6 +52,12 @@ const ContactState = props => {
   };
 
   // Delete Contact
+  const deleteContact = id => {
+    dispatch({
+      type: DELETE_CONTACT,
+      payload: id
+    });
+  };
 
   // Set Current Conatact
 
@@ -67,7 +73,8 @@ const ContactState = props => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
-        addContact
+        addContact,
+        deleteContact
       }}
     >
       {props.children}
