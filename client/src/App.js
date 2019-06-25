3ed5309ee2cef324e4b02151fa2c926e98ee526a
Gabriel Santiago <gisantiago@live.com>
2019-06-24 22:46:55 -0400

    Register & Login Forms

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 
 const App = () => {
   return (
@@ -19,6 +21,8 @@ const App = () => {
               <Switch>
                 <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/login' component={Login} />
               </Switch>
             </div>
           </Fragment>
