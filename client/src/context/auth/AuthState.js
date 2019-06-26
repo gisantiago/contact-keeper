3d2b390802deb970f1be71ffc96d37e06f269f61
gisantiago <gisantiago@live.com>
2019-06-26 11:01:08 -0400

    User Login

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -58,6 +58,31 @@ const AuthState = props => {
       const res = await axios.post('/api/users', formData, config);
 
       dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      });
+
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg
+      });
+    }
+  };
+
+  // Login User
+  const login = async formData => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    try {
+      const res = await axios.post('/api/auth', formData, config);
+
+      dispatch({
         type: REGISTER_SUCCESS,
         payload: res.data
       });
@@ -70,9 +95,6 @@ const AuthState = props => {
       });
     }
   };
-
-  // Login User
-  const login = () => console.log('login');
 
   // Logout
   const logout = () => console.log('logout');
