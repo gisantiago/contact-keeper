f683bd6518b28f4837feef7126a6407040f72399
Gabriel Santiago <gisantiago@live.com>
2019-06-26 22:56:05 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -4,13 +4,11 @@ const path = require('path');
 
 const app = express();
 
-// connectDB
+// Connect Database
 connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
-
-
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
@@ -27,6 +25,6 @@ if (process.env.NODE_ENV === 'production') {
   );
 }
 
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`App running on port:${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
